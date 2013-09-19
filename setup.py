b5497a7011e4e18b91227fbc06afe6b68c1203a6
eqy <Loser777@gmail.com>
2013-09-19 02:14:11 -0700

    fix Windows cx_freeze sc2reader non *.pyc dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-import sys
+from cx_Freeze import setup, Executable
 import os
 
-CONST_SC2READER_PATH = 'C:\Python33\Lib\site-packages\sc2reader\data'
+#There's no documentation on how to do this zip-include stuff, just posts by SO Gods
+CONST_SC2READER_PATH = 'C:\Python33\Lib\site-packages\sc2reader\data\\'
+CONST_ROOT = 'C:\Python33\Lib\site-packages\\'
 
-from cx_Freeze import setup, Executable
-
-build_exe_options = {"include_files": ['C:\Python33\Lib\site-packages\sc2reader\data\ ']}
-
+includefiles = list()
+for dirpath, dirnames, filenames in os.walk(CONST_SC2READER_PATH):
+    for filename in filenames:
+        includefiles.append([dirpath+'\\'+filename, os.path.relpath(dirpath+'\\'+filename,CONST_ROOT)])
+        print(includefiles[-1])
 
 
 setup(
-    name = "vroMAD",
-    version = "0.1.0",
-    executables = [Executable("__main__.py", base = "WIN32GUI")])
+     name = "vroMAD",
+     version = "0.1.0",
+     options = {'build_exe': {'zip_includes':includefiles}},
+     executables = [Executable("__main__.py", base = "WIN32GUI")])
