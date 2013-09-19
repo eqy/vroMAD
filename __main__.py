3689bfba4e088d9cd494c58cb6ea11838ce3dba6
eqy <Loser777@gmail.com>
2013-09-19 01:24:36 -0700

    Add tolerance for stdio free Windows builds

--- __main__.py
+++ __main__.py
@@ -1,9 +1,27 @@
+import sys
 import VroMADGUI
 import VroMAD
 from multiprocessing import freeze_support
 
 if __name__ == "__main__":
-    freeze_support() 
+    #Avoid nasty cx_freeze exception
+    try:
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+    except:
+        class fakeStream:
+            def __init__(self): pass
+            def write(self, data): pass
+            def read(self, data): pass
+            def flush(self): pass
+            def close(self): pass
+        sys.stdout = fakeStream()
+        sys.stderr = fakeStream()
+        sys.stdin  = fakeStream()
+        sys.__stdout__ = fakeStream()
+        sys.__stderr__ = fakeStream()
+        sys.__stdin__  = fakeStream()
+    freeze_support()	
     gui = VroMADGUI.VroMADGUI()
     #gui.populate()
     gui.paint()
--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import sys
+import os
+
+CONST_SC2READER_PATH = 'C:\Python33\Lib\site-packages\sc2reader\data'
 
 from cx_Freeze import setup, Executable
 
 build_exe_options = {"include_files": ['C:\Python33\Lib\site-packages\sc2reader\data\ ']}
+
+
 
 setup(
     name = "vroMAD",
