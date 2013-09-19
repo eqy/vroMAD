91cac41f828c0ac684c9a9e57e97e95946f829b5
eqy <Loser777@gmail.com>
2013-09-18 20:45:40 -0700

    windows gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+import sys
+
+from cx_Freeze import setup, Executable
+
+build_exe_options = {"include_files": ['C:\Python33\Lib\site-packages\sc2reader\data\ ']}
+
+setup(
+    name = "vroMAD",
+    version = "0.1.0",
+    executables = [Executable("__main__.py", base = "base")])
